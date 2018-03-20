384eb73c38daac51fdc635a2f7176bc8387573e0
ChetanBelur <cbelur@umass.edu>
2018-03-19 22:29:25 -0400

    Initial version.

--- unit_clause.sv
+++ unit_clause.sv
@@ -1,84 +1,68 @@
-timescale 1ns / 1ps
+`timescale 1ns / 1ps
 import common::*;
 
-module unitClause (clock, reset, find, in_formula, ended, found, lit_found);
+module Unit_Clause(input clock, reset,find,
+           input formula in_formula,
+           output ended, found, 
+           output lit lit_found);
+           
 
-input clock, reset, find;
-input formula in_formula;
-output ended, found;
-output lit lit_found;
-
-formula s_in_formula = ZERO_FORMULA;
-logic s_ended = 0;
-logic s_found = 0;
-lit s_lit_found = ZERO_LIT;
-logic s_finding = 0;
-int i = 0;
-
+formula s_in_formula;
+logic s_ended;
+logic s_found;
+lit s_lit_found;
+logic s_finding;
+logic[width_clausearray:0] i;
 
 assign ended = s_ended;
 assign found = s_found;
 assign lit_found = s_lit_found;
 
-always_ff @(posedge clock or posedge reset)
-begin
-	
-if reset
-	begin
-		s_ended <= 0;
-		s_found <= 0;
-	end
-	
-	
+always_ff@(posedge clock or posedge reset)
+if (reset)
+    begin
+    s_ended <= 1'b0;
+    s_found <= 1'b0;
+    s_in_formula <= zero_formula;
+    s_lit_found<= zero_lit;
+    s_finding <= 1'b0;
+    i <= 1'b0;
+    end
+
 else if (clock)
-	begin
-		s_ended <= 0;
-
-		if ((find == 1) and (s_finding == 0))
-			begin
-				s_in_formula <= in_formula;
-				s_finding <= 1;
-				s_ended <= 0;
-				i <= 0;
-			end
-		else if (s_finding == 1)
-			begin
-				for (i; i < NUMBER_CLAUSES; i++)
-					begin
-						if ((i == s_in_formula.len) or (_finished == 1))
-							break;
-						if ((s_in_formula.clauses(i)).len == 1)
-							begin
-								_found <= 1;
-								_finished <= 1;
-								s_lit_found <= (s_in_formula.clauses(i)).lits[0];
-							end
-					end
-				s_ended <= 1;
-
-				if ((i < s_in_formula.len) and (i < NUMBER_CLAUSES))
-					begin
-						if (s_in_formula.clauses(i).len = 1)
-							begin
-								s_finding <= 0;
-								s_found <= 1;
-								s_ended <= 1;
-								s_lit_found <= s_in_formula.clauses(i).lits(0);
-							end
-						else
-							s_ended <= 0;
-
-					end
-				else
-					begin
-						s_finding <= 0;
-						s_ended <= 0;
-						s_found <= 0; 
-					end
-
-				i <= i+1;
-			end
-	end
-
-						
+    begin
+	s_ended <= 1'b0;
+	if (find==1'b1 & s_finding==1'b0)
+	   begin
+		s_in_formula <= in_formula;
+		s_finding <= 1'b1;
+		s_ended <= 1'b0;
+		i <= 1'b0;
+	   end
+	else if (s_finding == 1'b1)
+        begin
+		if((i < s_in_formula.len) & (i < number_clauses))
+		    begin 
+			if (s_in_formula.clauses[i].len == 1'b1)
+				begin
+				s_finding <= 1'b0;
+				s_found <= 1'b1;
+				s_ended <= 1'b1;
+				s_lit_found <= s_in_formula.clauses[i].lits[0];
+				end
+			else
+			    begin
+				s_ended <= 1'b0;
+			    end
+		    end
+		else
+		    begin
+			s_finding <= 1'b0;
+			s_ended <= 1'b1;
+			s_found <= 1'b0; 
+		    end
+		i <= i+1;     
+        end
+       
+    end
 endmodule

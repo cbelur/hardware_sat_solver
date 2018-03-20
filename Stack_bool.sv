06cd5c514ac3eb287d401c068f47df2dc15940b4
ChetanBelur <cbelur@umass.edu>
2018-03-20 12:30:29 -0400

    Initial version_tested

--- Stack_bool.sv
+++ Stack_bool.sv
@@ -1,73 +1,68 @@
+`timescale 1ns / 1ps
+
 import common::*;
 
-module Stack_bool (clock, reset, wr_en, pop, din, dout, front, full, empty);
+module Stack_bool(input clock, reset, wr_en, pop, din,
+                  output dout, front, full, empty);
+                  
+typedef logic[bool_stack_size-1:0] mem_type;
+mem_type data;
+logic[width_bool_stack_size:0] curr_size;
+logic[width_bool_stack_size:0] temp;
 
-input clock, reset, wr_en, pop, din;
-output dout, front, full, empty;
+logic data_in, data_out, full_signal, empty_signal, front_signal;
 
-
-reg [bool_stack_size-1:0] data = 0;
-int curr_size = 0;
-
-wire data_in, data_out;
-
-reg full_signal, empty_signal;
-int temp;
-
-assign data_in <= din;
-assign dout <= data_out;
-assign full <= full_signal;
-assign empty <= empty_signal;
-
-always @(posedge clock or posedge reset)
-begin
-	temp <= curr_size;
-	
-	if (reset==1)
-	begin
-		data <= 0;
-		curr_size <= 0;
-		data_out <= 0;
-		empty_signal <= 1;
-		full_signal <= 1;
-		front <= 0;
-	end
-	
-	else
-	begin
-		if ((wr_en == 1) and (full_signal == 0))
-		begin
-			data[curr_size] <= data_in;
-			curr_size <= curr_size + 1;
-			temp <= temp + 1;
-		end
-		
-		else if ((pop == 1) and (empty_signal == 0))
-		begin
-			data_out <= data[curr_size-1];
-			curr_size <= curr_size - 1;
-			temp <= temp - 1;
-		end
-		
-		if temp > 0
-			front <= data[temp-1];
-			
-		if (temp == bool_stack_size)
-		begin
-			full_signal <= 1;
-			empty_signal <= 1;
-		end
-		else if (temp==0)
-		begin
-			full_signal <= 0;
-			empty_signal <= 1;
-		end
-		else
-		begin
-			full_signal <= 0;
-			empty_signal <= 0;
-		end
-	end
-end
-
+assign data_in = din;
+assign dout = data_out;
+assign full = full_signal;
+assign empty = empty_signal;
+assign front = front_signal;
+always_ff@(posedge clock or posedge reset)
+if (reset)
+    begin
+    data <= 1'b0;
+    curr_size <= 1'b0;
+    data_out <= 1'b0;
+    empty_signal <= 1'b1;
+    full_signal <= 1'b0;
+    front_signal <= 1'b0;
+    end
+else if (clock)
+    begin
+    //PUSH
+    if (wr_en == 1'b1 & full_signal == 1'b0)
+        begin
+        data[curr_size] <= data_in;
+        curr_size <= curr_size+1;
+        front_signal <= data_in;
+        empty_signal <= 1'b0;
+        if (curr_size+1 >= bool_stack_size)
+            begin
+            full_signal <= 1'b1;
+            end
+        else
+            begin
+            full_signal <= 1'b0;
+            end
+        end
+    //POP
+     else if (pop == 1'b1 & empty_signal == 1'b0)
+        begin
+        if (curr_size > 1'b0)
+            begin
+            curr_size <= curr_size-1;
+            data_out <= data[curr_size-1];
+            end
+        front_signal <= data[curr_size-2];
+        full_signal <= 1'b0;
+        if (curr_size == 1'b0)
+            begin
+            empty_signal <= 1'b1;
+            end
+        else
+            begin
+            empty_signal <= 1'b0;
+            end
+        end
+    end
 endmodule
